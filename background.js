6705284ac48dc0fae77d5b5b6b488d11ca8a1232
5odead <71369187+5odead@users.noreply.github.com>
2026-03-07 13:55:26 +0530

    Refactor background.js for clarity and functionality

--- background.js
+++ background.js
@@ -1,13 +1,18 @@
+//state
 let isEnabled = true;
 let blacklist = [];
-// empty start
 
+//keep Service Worker alive
+const keepAlive = () => setInterval(chrome.runtime.getPlatformInfo, 20000);
+chrome.runtime.onStartup.addListener(keepAlive);
+keepAlive();
+
+//load blacklist
 async function loadBlackList() {
     try {
         const response = await fetch("blacklist.json");
         const data = await response.json();
-        blacklist = data.domains;
-// extract domains
+        blacklist = data.domains; // extract domains
         console.log("Knight: BlackList Loaded", blacklist.length, "Domains");
     }
     catch (error) {
@@ -15,39 +20,45 @@ async function loadBlackList() {
         blacklist = []
     }
 }
-
 loadBlackList();
 
+//Check if URL is Blacklisted
 function isBlacklisted(url) {
     try {
         const hostname = new URL(url).hostname.replace("www.","");
-// Disects url and extract hostnames then remove the www. part
+        // Disects url and extract hostnames then remove the www. part
         return blacklist.some(domain => hostname === domain || hostname.endsWith("." + domain));
-// iterate over every domains and check if its in blacklist or is a subdomain of blacklisted
+        // iterate over every domains and check if its in blacklist or is a subdomain of blacklisted
     }
     catch (error) {
         return false
     }
 }
 
+//navigation listener
 chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-// chrome api with event
     if (details.frameId !== 0) return;
     console.log("Knight: isEnabled is", isEnabled);
     if (!isEnabled) {
-            console.log("Knight: protection is off, allowing URL");
-    return;
+        console.log("Knight: protection is off, allowing URL");
+        return;
     }
     if (isBlacklisted(details.url)) {
         chrome.tabs.update(details.tabId, { url: chrome.runtime.getURL("blocked.html") + "?url=" + encodeURIComponent(details.url)
-// change current tab to blocked.html and encode the phishing url
+        // change current tab to blocked.html and encode the phishing url
         });
     }
 });
 
-chrome.runtime.onMessage.addListener(function(message){
+//message Listener
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
     if (message.action === "setEnabled") {
         isEnabled = message.value;
         console.log("Knight: isEnabled set to", isEnabled);
     }
+    if (message.action === "isBlacklisted") {
+        const result = isBlacklisted(message.url);
+        sendResponse({ result: result });
+    }
+    return true;
 });
